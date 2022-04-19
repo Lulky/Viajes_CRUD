4efa8fe0629d9af51f546311acd631d911b74ba4
unknown <lukyluxiano.12@gmail.com>
2022-04-19 13:56:57 -0500

    Viajes

---
+++ src/main/java/com/codingdojo/luciano/repositorios/RepositorioViajes.java
@@ -0,0 +1,19 @@
+package com.codingdojo.luciano.repositorios;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.codingdojo.luciano.modelos.Gasto;
+
+@Repository
+public interface RepositorioViajes extends CrudRepository<Gasto, Long>{
+	
+	List<Gasto> findAll();
+	List<Gasto> findById(long id);
+	
+	Gasto save(Gasto gasto);
+	
+	void deleteById(Long id);
+}
